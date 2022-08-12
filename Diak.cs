f9e35cf85bcfc1dc26e2dd9616b9a761b40336cf
László Kállai <kallailaszlo06@gmail.com>
2022-08-12 12:11:49 +0200

    Add project files.

---
+++ Diak.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Karesz
+{
+    public partial class Form1 : Form
+    {
+        void DIÁK_ROBOTJAI()
+        {
+            Robot karesz = Robot.Get("Karesz");
+
+            karesz.Feladat = delegate ()
+            {
+                karesz.Lépj();
+                karesz.Fordulj(balra);
+                while (karesz.UltrahangSzenzor() != 1)
+                {
+                    karesz.Lépj();
+                }
+                karesz.Fordulj(balra);
+                karesz.Várj();
+                karesz.Várj();
+                karesz.Lépj();
+                karesz.Fordulj(jobbra);
+                karesz.Lőjj();
+
+                for (int i = 0; i < 20; i++)
+                {
+                    karesz.Lépj();
+                }
+                karesz.Várj();
+                while (karesz.UltrahangSzenzor() != 2)
+                {
+                    karesz.Lépj();
+                }
+                karesz.Fordulj(jobbra);
+                int b, k, j;
+                (b, k, j) = karesz.SzélesUltrahangSzenzor();
+                while (karesz.UltrahangSzenzor() > 2)
+                {
+                    if ((b != 1 && k != 1) || (b == 1 && j == 1))
+                    {
+                        karesz.Lépj();
+                        (b, k, j) = karesz.SzélesUltrahangSzenzor();
+                    }
+                }
+                karesz.Fordulj(jobbra);
+                for (int i = 0; i < 2; i++)
+                {
+                    Vegyél_fel_egy_kavicsot();
+                    Lépj();
+                }
+                karesz.Vegyél_fel_egy_kavicsot();
+                karesz.Fordulj(balra);
+                karesz.Lépj();
+                karesz.Fordulj(balra);
+                for (int i = 0; i < 3; i++)
+                {
+                    Vegyél_fel_egy_kavicsot();
+                    Lépj();
+                }
+                karesz.Vegyél_fel_egy_kavicsot();
+                karesz.Fordulj(balra);
+                karesz.Lépj();
+                karesz.Vegyél_fel_egy_kavicsot();
+            };
+        }
+    }
+}
+
+/* LEGFONTOSABB PARANCSOK
+
+MOZGÁSOK
+
+karesz.Lépj();                          -------- Karesz előre lép egyet.
+karesz.Fordulj(jobbra);                 -------- Karesz jobbra fordul.
+karesz.Fordulj(balra);                  -------- Karesz balra fordul.
+karesz.Vegyél_fel_egy_kavicsot();       -------- Karesz felvesz egy kavicsot.
+karesz.Tegyél_le_egy_kavicsot();        -------- Karesz letesz egy fekete kavicsot
+karesz.Tegyél_le_egy_kavicsot(piros);   -------- Karesz letesz egy piros kavicsot.
+
+Minden mozgás után a robot köre véget ér és a következő robot jön. 
+
+
+
+SZENZOROK
+
+karesz.Előtt_fal_van();                 -------- igaz, ha Karesz fal előtt áll, egyébként hamis.
+karesz.Ki_fog_lépni_a_pályáról();       -------- igaz, ha Karesz a pálya szélén kifele néz, egyébként hamis.
+karesz.Alatt_van_kavics();              -------- igaz, ha Karesz épp kavicson áll, egyébként hamis.
+karesz.Köveinek_száma_ebből(piros)      -------- Karesz piros köveinek a száma.
+karesz.Alatt_ez_van();                  -------- a kavics színe, amin Karesz áll. (Ez igazából egy szám!)
+karesz.UltrahangSzenzor();              -------- a Karesz előtt található tárgy távolsága. Ez a tárgy lehet fal vagy másik robot is. 
+karesz.SzélesUltrahangSzenzor();        -------- ugyanaz, mint az ultrahangszenzor, de ez nem csak a Karesz előtti mezőket pásztázza, hanem a szomszédos mezőket is. Egy számhármast ad vissza. 
+karesz.Hőmérő();                        -------- a Karesz által mért hőmérséklet. A láva forrása 1000 fok, amitől lépésenként távolodva a hőmérséklet 200 fokonként hűl. Az alapértelmezett hőmérséklet 0 fok.
+
+A szenzorokat bármennyiszer használhatja a robot a saját körén belül.
+
+*/
---
+++ Tanar.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace Karesz
+{
+    public partial class Form1 : Form
+    {
+        string betöltendő_pálya = "palya008.txt";
+
+        void TANÁR_ROBOTJAI()
+        {
+            new Robot("Karesz", 10, 10, 10, 10, 5, 1, 15, 2);
+
+
+            Robot janesz = new Robot("Janesz", Robot.képkészlet_lilesz, 0, 0, 0, 0, 1500, 35, 13, 2);
+
+
+            janesz.Feladat = delegate ()
+            {
+                while (pálya.MiVanItt(new Vektor(39, 29)) == sárga)
+                {
+                    janesz.Lőjj();
+                    for (int j = 0; j < 3; j++)
+                    {
+                        janesz.Várj();
+                    }
+
+                }
+            };
+
+            Robot paresz = new Robot("paresz", Robot.képkészlet_lilesz, 0, 0, 0, 0, 1500, 35, 17, 1);
+
+
+            paresz.Feladat = delegate ()
+            {
+                while (pálya.MiVanItt(new Vektor(39, 29)) == sárga)
+                {
+                    paresz.Lőjj();
+                    for (int j = 0; j < 6; j++)
+                    {
+                        paresz.Várj();
+                    }
+
+                }
+            };
+
+            Robot matyesz = new Robot("matyesz", Robot.képkészlet_lilesz, 0, 0, 0, 0, 1500, 15, 15, 1);
+
+            matyesz.Feladat = delegate ()
+            {
+                int a = 0;
+                while (pálya.MiVanItt(new Vektor(39, 29)) == sárga)
+                {
+
+                    for (int i = 0; i < 8; i++)
+                    {
+                        matyesz.Lépj();
+                        a = matyesz.UltrahangSzenzor();
+                        if (a < 8)
+                        {
+                            matyesz.Lőjj();
+                            for (int j = 0; j < 4; j++)
+                            {
+                                matyesz.Várj();
+                            }
+                        }
+                    }
+                    matyesz.Fordulj(balra);
+                    matyesz.Fordulj(balra);
+
+                }
+            };
+
+        }
+    }
+}
